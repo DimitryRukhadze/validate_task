de37a8aa7c410d9130472f744cd507c14ec3d793
Юрченко Евгений <71762690+boo-learn@users.noreply.github.com>
2023-11-29 02:28:06 +0700

    Update validate_task_string.py
    рекомендации

--- validate_task_string.py
+++ validate_task_string.py
@@ -1,10 +1,11 @@
 
 def get_ver_num(ver_str: str) -> int:
-    return int(ver_str[:1])
+    return int(ver_str[0])
 
 
 def first_eq_last(ver_str: str) -> bool:
-    check_str: str = ''
+    check_str = ''
+    # TODO: что тут происходит, вообще непонятно
     for index, symb in enumerate(ver_str):
         if not symb.isdigit():
             check_str = ver_str[index:]
@@ -14,11 +15,14 @@ def first_eq_last(ver_str: str) -> bool:
 
 
 def has_correct_length(ver_str: str) -> bool:
+    # TODO: так тожде писать не нужно
+    # return len(ver_str) == 10 в все -)
     if len(ver_str) == 10:
         return True
     return False
 
 
+# TODO: это глоьбальной переменной - плохая идея. Лучше локальной в validate_ver()
 ver_protocol = {
     1: [
         first_eq_last,
@@ -27,7 +31,9 @@ ver_protocol = {
 }
 
 
+# TODO: Такое себе название, ты валидируешь строку, а не ее версию
 def validate_ver(task_str: str) -> bool:
+    # TODO: тут нужна обработка исключения, если первый символ не цифра
     ver_num = get_ver_num(task_str)
     for func in ver_protocol[ver_num]:
         if not func(task_str):
